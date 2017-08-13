1b572d7cf25bb317dd38c065ee9f00f929d0006a
nitpum <nitpum@outlook.com>
2017-08-13 19:33:48 +0700

    Fix script to correct with eslint rule

--- youtube.js
+++ youtube.js
@@ -1,24 +1,24 @@
-Module.register('youtube', {
-    defualts : {
-        autoplay: false,
-        showplayercontrol : true,
-        showsuggestedvideo : false,
-        showtitle : false,
-        width: 560,
-        height: 315,
-        watch : "",
-    },
-    getDom: function () {
-        var wrapper = document.createElement('div');
-        var GET = "?";
-        
-        // autoplay
-        GET += (this.config.autoplay) ? "autoplay=1" : "autoplay=0";
-        GET += (this.config.showplayercontrol)? "&controls=1":"&controls=0";
-        GET += (this.config.showsuggestedvideo)? "&rel=1":"&rel=0";
-        GET += (this.config.showtitle)? "&showinfo=1":"&showinfo=0";
+Module.register("youtube", {
+	defualts: {
+		autoplay: false,
+		showplayercontrol : true,
+		showsuggestedvideo : false,
+		showtitle : false,
+		width: 560,
+		height: 315,
+		watch : "",
+	},
+	getDom: function () {
+		var wrapper = document.createElement("div");
+		var GET = "?";
 
-        wrapper.innerHTML = '<iframe width="' + this.config.width +'" height="' + this.config.height + '" src="https://www.youtube.com/embed/' + this.config.watch + GET +'" frameborder="0" allowfullscreen></iframe>';
-        return wrapper;
-    }
+        // autoplay
+		GET += (this.config.autoplay) ? "autoplay=1" : "autoplay=0";
+		GET += (this.config.showplayercontrol)? "&controls=1":"&controls=0";
+		GET += (this.config.showsuggestedvideo)? "&rel=1":"&rel=0";
+		GET += (this.config.showtitle)? "&showinfo=1":"&showinfo=0";
+
+		wrapper.innerHTML = "<iframe width=\"" + this.config.width +"\" height=\"" + this.config.height + "\" src=\"https://www.youtube.com/embed/" + this.config.watch + GET +"\" frameborder=\"0\" allowfullscreen></iframe>";
+		return wrapper;
+	}
 });
