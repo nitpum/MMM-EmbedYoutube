f4adb4a029a1f79f916ea2afc7e8e9618490a631
nitpum <nitpum@outlook.com>
2018-10-15 02:32:51 +0700

    Supported playlist and fix custom playlist

--- MMM-EmbedYoutube.js
+++ MMM-EmbedYoutube.js
@@ -21,21 +21,26 @@ Module.register("MMM-EmbedYoutube", {
 		rel : false,
 		showinfo : false,
 		video_id : "",
+		playlist: "",
 		video_list: []
 	},
 	getDom: function () {
 		var wrapper = document.createElement("div");
 
 		// Parameter
-		var params = "?";
+		var params = "";
 
 		var videoList = "";
 		if (this.config.video_list && this.config.video_list.length > 0) {
 			videoList = "&playlist=";
-			for (var i = 0; i < this.config.video_list.length; i++) videoList += this.config.video_list[i];
+			for (var i = 0; i < this.config.video_list.length; i++) {
+				videoList += this.config.video_list[i];
+				if (i + 1 < this.config.video_list.length)
+					videoList += ",";
+			}
 		}
 		params += (this.config.autoplay) ? "autoplay=1" : "autoplay=0";
-		params += (this.config.color != "red")? "&color=" + this.config.color:"";
+		params += (typeof this.config.color !== "undefined" && this.config.color != "red")? "&color=" + this.config.color:"";
 		params += (this.config.controls)? "&controls=1":"&controls=0";
 		params += (this.config.disablekb)? "&disablekb=1":"";
 		params += (this.config.fs)? "":"&fs=0";
@@ -45,7 +50,11 @@ Module.register("MMM-EmbedYoutube", {
 		params += (this.config.rel)? "&rel=1": "&rel=0";
 		params += (this.config.showinfo)? "&showinfo=1": "&showinfo=0";
 
-		wrapper.innerHTML = "<iframe width=\"" + this.config.width +"\" height=\"" + this.config.height + "\" src=\"https://www.youtube.com/embed/" + this.config.video_id + params +"\" frameborder=\"0\" allowfullscreen></iframe>";
+		var videoId = this.config.video_id +"?";
+		if (typeof this.config.playlist !== "undefined" && this.config.playlist != "")
+			videoId = "playlist?list=" + this.config.playlist + "&";
+
+		wrapper.innerHTML = "<iframe width=\"" + this.config.width +"\" height=\"" + this.config.height + "\" src=\"https://www.youtube.com/embed/" + videoId + params +"\" frameborder=\"0\" allowfullscreen></iframe>";
 		return wrapper;
 	}
 });
